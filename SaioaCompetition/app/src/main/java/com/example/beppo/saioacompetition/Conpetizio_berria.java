7289f448ce14a1dc3ce1ffe167ffb26003d1fcca
ElBeppo <eamarkel@gmail.com>
2017-03-18 19:34:12 +0100

    Conpetizio berria layout

---
+++ SaioaCompetition/app/src/main/java/com/example/beppo/saioacompetition/Conpetizio_berria.java
@@ -0,0 +1,13 @@
+package com.example.beppo.saioacompetition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Conpetizio_berria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conpetizio_berria);
+    }
+}
